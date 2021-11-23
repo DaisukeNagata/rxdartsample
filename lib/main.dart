902a335c317acd2d156dfa315a9b5f30db16be58
大祐永田 <dbank0208@gmail.com>
2021-11-23 21:45:37 +0900

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:rxdart/rxdart.dart';
 
-class ProductModel {
-  String productName, productPrice;
-  int productName2, productPrice2;
+class CountModel {
+  int count;
 
-  ProductModel(this.productName, this.productPrice, this.productName2,
-      this.productPrice2);
+  CountModel(this.count);
 }
 
 class ProductListModel {
-  List<ProductModel> products = [];
+  List<CountModel> products = [];
 
-  void add(String st) {
-    int count = int.parse(st) + 1;
-    products.add(ProductModel(st, count.toString(), 3, 4));
+  void add(int index) {
+    products.add(CountModel(index));
   }
 }
 
 void main() {
-  MyApp().myapp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  var subject = PublishSubject<String>();
-  var p = ProductListModel();
-
-  void myapp() async {
-    p.add("1");
-
-    // 購読
-    subject.listen((data) {
-      print('検知1------');
-      print(data);
-    });
-
-    // 発行
-    subject.add(p.products.last.productName);
-    subject.add(p.products.last.productPrice);
-    subject.add(p.products.last.productName2.toString());
-    subject.add(p.products.last.productPrice2.toString());
-
-    await Future.delayed(Duration(seconds: 5)); // このコードは検証用 5秒待つ
-    print('【ここまで発行1回目】');
-    // 購読
-    subject.listen((data) {
-      print('検知2------');
-      print(data);
-    });
-    subject.listen((data) {
-      print('検知3------');
-      print(data);
-    });
-    subject.add("Item3");
-    await Future.delayed(Duration(seconds: 5)); // このコードは検証用 5秒待つ
-    print('【ここまで発行2回目】');
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // ⑤
       home: MyHomePage(),
     );
   }
@@ -74,30 +35,61 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  int count = 0;
+  var subject = PublishSubject<int>();
+  var productListModel = ProductListModel();
 
   void _incrementCounter() {
     setState(() {
-      _counter++;
+      // 購読
+      subject.listen((data) {
+        _counter = data;
+      });
+      count = count += 1;
+      productListModel.add(count);
+      // 発行
+      subject.add(productListModel.products.last.count);
+    });
+  }
+
+  void reset() {
+    setState(() {
+      subject.add(0);
+      count = 0;
+      productListModel.products = [];
     });
   }
 
   @override
+  void dispose() {
+    subject.close();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // ④
     return Scaffold(
-      // ③
       body: Center(
-        // ②
         child: Text(
-          // ①
           '$_counter',
-          style: Theme.of(context).textTheme.headline4,
+          style: Theme
+              .of(context)
+              .textTheme
+              .headline4,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          FloatingActionButton(
+            onPressed: _incrementCounter,
+            child: Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: reset,
+            child: Icon(Icons.close),
+          ),
+        ],
+      ),
     );
   }
 }
